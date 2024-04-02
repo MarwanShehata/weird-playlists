6f14799a9f84553639b75ceef7eacb3d10c0b4ed
Marwan Shehata <marwanzeitoun1@gmail.com>
2024-04-02 12:48:37 +0200

    Optimize data consumption by specifying query fields - Implemented query fields in the Spotify API request to reduce data consumption from 301KB to 70KB. - This optimization was tested and verified using Postman.

--- src/js/app.js
+++ src/js/app.js
@@ -7,164 +7,133 @@
 const clientId = import.meta.env.VITE_CLIENTID;
 const clientSecret = import.meta.env.VITE_CLIENTSECRET;
 const playlistId = '6XVNcaZKzxdErXvCj9JCEe';
+const fields = 'external_urls.spotify,images.url,id,description,owner.display_name,owner.external_urls.spotify,tracks.items(track(external_urls.spotify,album.images.url,preview_url,name,id,track_number),track.artists(name,external_urls.spotify,id))';
+/* BY USING FIELDS I REDUCED THE DATA CONSUMPTION FROM 301KB TO 70KB (TESTED WITH Postman) */
 
 let accessToken;
 
 const basicAuth = async () => {
-    const authOptions = {
-        method: 'POST',
-        headers: {
-            Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: 'grant_type=client_credentials',
-    };
+  const authOptions = {
+    method: 'POST',
+    headers: {
+      Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: 'grant_type=client_credentials',
+  };
 
-    const response = await fetch(
-        'https://accounts.spotify.com/api/token',
-        authOptions,
-    );
-    const data = await response.json();
-    accessToken = data.access_token;
+  const response = await fetch(
+    'https://accounts.spotify.com/api/token',
+    authOptions,
+  );
+  const data = await response.json();
+  accessToken = data.access_token;
 };
 
 const fetchTracks = async (playlistId, accessToken) => {
-    try {
-        const fetchOptions = {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${accessToken}`,
-            },
-        };
-        const response = await fetch(
-            `https://api.spotify.com/v1/playlists/${playlistId}/`,
-            fetchOptions,
-        );
+  try {
+    const fetchOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    };
+    const response = await fetch(
+      `https://api.spotify.com/v1/playlists/${playlistId}?fields=${encodeURIComponent(fields)}`,
+      fetchOptions,
+    );
 
-        if (!response.ok) {
-            throw new Error('Failed to fetch tracks');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error(error);
-        return null;
+    if (!response.ok) {
+      throw new Error('Failed to fetch tracks');
     }
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
 /* Handling Data */
 const handleData = async () => {
-    try {
-        await basicAuth();
-        const fetchedData = await fetchTracks(playlistId, accessToken);
-        console.log(fetchedData);
-        if (fetchedData) {
-            const playlistInfo = processPlaylistInfo(fetchedData);
-            const tracksInfo = processTracks(fetchedData);
-            return { playlistInfo, tracksInfo };
-        } else {
-            console.error(`Failed to fetch the tracks`);
-            return null;
-        }
-    } catch (error) {
-        console.error(error);
-        return null;
+  try {
+    await basicAuth();
+    const fetchedData = await fetchTracks(playlistId, accessToken);
+    console.log(fetchedData);
+    if (fetchedData) {
+      const playlistInfo = processPlaylistInfo(fetchedData);
+      const tracksInfo = processTracks(fetchedData);
+      return { playlistInfo, tracksInfo };
+    } else {
+      console.error(`Failed to fetch the tracks`);
+      return null;
     }
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 };
 
 /* Tidying up the response */
 const processTracks = (data) => {
-    let tracksInfo = [];
+  let tracksInfo = [];
 
-    data.tracks.items.forEach((item) => {
-        /* <Misc> */
-        const trackSpotifyLink = item.track.external_urls.spotify;
-        const previewThumbnailLink = item.track.album.images[0].url;
-        const previewUrl = item.track.preview_url;
-        /* Tracks */
-        const trackName = item.track.name;
-        const trackId = item.track.id;
-        const trackNumber = item.track.track_number;
-        /* Artists */
-        const artistName = item.track.artists[0].name;
-        const artistUrl = item.track.artists[0].external_urls.spotify;
-        const artistId = item.track.artists[0].id;
+  data.tracks.items.forEach((item) => {
+    /* <Misc> */
+    const trackSpotifyLink = item.track.external_urls.spotify;
+    const previewThumbnailLink = item.track.album.images[0].url;
+    const previewUrl = item.track.preview_url;
+    /* Tracks */
+    const trackName = item.track.name;
+    const trackId = item.track.id;
+    const trackNumber = item.track.track_number;
+    /* Artists */
+    const artistName = item.track.artists[0].name;
+    const artistUrl = item.track.artists[0].external_urls.spotify;
+    const artistId = item.track.artists[0].id;
 
-        tracksInfo.push({
-            trackSpotifyLink,
-            previewThumbnailLink,
-            previewUrl,
-            trackName,
-            trackId,
-            trackNumber,
-            artistName,
-            artistUrl,
-            artistId,
-        });
+    tracksInfo.push({
+      trackSpotifyLink,
+      previewThumbnailLink,
+      previewUrl,
+      trackName,
+      trackId,
+      trackNumber,
+      artistName,
+      artistUrl,
+      artistId,
     });
-    console.log(tracksInfo);
-    return tracksInfo;
+  });
+  console.log(tracksInfo);
+  return tracksInfo;
 };
 
 const processPlaylistInfo = (data) => {
-    let playlistInfo = [];
+  let playlistInfo = [];
 
-    /* <Misc> */
-    const playlistSpotifyLink = data.external_urls.spotify;
-    const previewThumbnailLink = data.images[0].url;
-    const playlistID = data.id;
-    const playlistDescription = data.description;
-    /* Owner */
-    const ownerName = data.owner.display_name;
-    const ownerURL = data.owner.external_urls.spotify;
+  /* <Misc> */
+  const playlistSpotifyLink = data.external_urls.spotify;
+  const previewThumbnailLink = data.images[0].url;
+  const playlistID = data.id;
+  const playlistDescription = data.description;
+  /* Owner */
+  const ownerName = data.owner.display_name;
+  const ownerURL = data.owner.external_urls.spotify;
 
-    playlistInfo.push({
-        playlistSpotifyLink,
-        previewThumbnailLink,
-        playlistID,
-        playlistDescription,
-        ownerName,
-        ownerURL,
-    });
-    console.log(playlistInfo);
-    return playlistInfo;
+  playlistInfo.push({
+    playlistSpotifyLink,
+    previewThumbnailLink,
+    playlistID,
+    playlistDescription,
+    ownerName,
+    ownerURL,
+  });
+  console.log(playlistInfo);
+  return playlistInfo;
 };
 
 const processAndDisplayTracks = async () => {
-    const { tracksInfo, playlistInfo } = await handleData();
-    console.log(tracksInfo);
-    console.log(playlistInfo);
+  const { tracksInfo, playlistInfo } = await handleData();
+  console.log(tracksInfo);
+  console.log(playlistInfo);
 };
 processAndDisplayTracks();
-
-////////////////////////////////////////////////////////////////////
-// Render tracks to HTML
-// const renderTracks = (tracks, elementId) => {
-//   const trackList = document.getElementById(elementId);
-
-//   if (!trackList) {
-//     console.error(`No element with ID ${elementId} found`);
-//     return;
-//   }
-
-//   tracks.items.forEach(({ track }) => {
-//     const listItem = document.createElement('li');
-//     listItem.textContent = track.name;
-//     trackList.appendChild(listItem);
-//   });
-// };
-
-// // Main function
-// const main = async () => {
-//   const playlistId = '0ZXVX604hmghJgqWCMsqcU';
-//   const accessToken = '85e2ffd5076a48949631f2fd57bb9676';
-
-//   const tracks = await fetchTracks(playlistId, accessToken);
-
-//   if (tracks) {
-//     renderTracks(tracks, 'track-list');
-//   } else {
-//     console.error('Failed to fetch or render tracks');
-//   }
-// };
-
-// main();
